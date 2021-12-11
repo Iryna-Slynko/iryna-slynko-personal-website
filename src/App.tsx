73b46107395bd6f9484d94e4039484fd92c94522
Iryna Slynko <iryna.slynko@gmail.com>
2021-12-11 00:02:54 +0000

    Copy information from previous website

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello World!         
+          My name is Iryna Slynko. I am a final year computing student at Technological University Dublin, on track for 2:1 BSc Hons degree,
+          and a Software Development student at National College of Ireland, studying towards Higher Diploma in Science in Software Development (Java).
+        <br/>
+          I have strong skills in Python, C# and SQL as well as learning Java, JavaScript, Node.js and Flask in current studies and upskilling to become a Full-Stack Software Engineer.
+        <br/>
+          I possess excellent communication, people and empathy skills with a Bachelors degree in Psychology. I am a great team player as well as initiative individual contributor.
+        <br/>
+          I love elevating others and take pride in my high work ethics.
       </header>
     </div>
   );

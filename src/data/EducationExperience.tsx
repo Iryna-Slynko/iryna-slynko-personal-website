c30c9664aade5f8be0fd1bb7d28112fdb02fcc21
Iryna Slynko <iryna.slynko@gmail.com>
2021-12-12 17:37:32 +0000

    Prepare modules data structures

--- src/data/EducationExperience.tsx
+++ src/data/EducationExperience.tsx
@@ -3,23 +3,30 @@ interface IEducationExperience {
   readonly website: URL,
   readonly diploma: string,
   readonly graduationDate: Date,
-  readonly subjects: string[]
+  readonly modules: IModule[]
+}
+
+interface IModule {
+  title: string,
+  grade: "A+" | "A" | "A-" | "B+" | "B" | "B-"
 }
 
 
 export default function EducationExperience(): IEducationExperience[] {
-  return [{
+  return [
+    {
+      universityName: "Technological University Dublin",
+      website: new URL("https://www.tudublin.ie/"),
+      diploma: "Bachelor of Science with Honours in Computing with IT Management",
+      graduationDate: new Date(2022, 9, 30),
+      modules: []
+    },
+    {
     universityName: "National College of Ireland",
     website: new URL("https://www.ncirl.ie/"),
     diploma: "Higher Diploma in Science in Software Development",
     graduationDate: new Date(2022, 3, 30),
-    subjects: []
-  },
-  {
-    universityName: "Technological University Dublin",
-    website: new URL("https://www.tudublin.ie/"),
-    diploma: "Bachelor of Science with Honours in Computing with IT Management",
-    graduationDate: new Date(2022, 9, 30),
-    subjects: []
-  }];
+    modules: []
+  }
+];
 }

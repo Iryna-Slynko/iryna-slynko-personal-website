e4e9170c4609810bab775518935c0b00bc608885
Iryna Slynko <iryna.slynko@gmail.com>
2021-12-13 23:10:50 +0000

    Show skills and highlight them

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -25,8 +25,29 @@ export default class Work extends React.Component<IProps> {
           date += format(work.endDate, "MMM yyyy");
       }
       const selected = selectedSkill != null && work.technologies.includes(selectedSkill);
+      const practices = work.practices.map(function(practice){
+        return <li key={practice}>
+          {practice}
+        </li>;
+      }).concat(work.technologies.map(function(technology){
+        return <li key={technology} className={technology === selectedSkill ? "selected-skill" : ""}>
+          {technology}
+        </li>;
+      })).sort((a: JSX.Element, b: JSX.Element) =>{
+        return (a.key !== null && b.key !== null) ? a.key.toString().localeCompare(b.key.toString()) : 0;
+      });
+
+
       return <MyAccordionItem key={work.companyName + selected ? "selected" : ""} selected={selected} date={date} subtitle={work.position} title={work.companyName} index={index} prefix='work'>
+        <p>
         {work.responsibilities}
+        </p>
+        <h6>
+        Practices and Technologies:
+        </h6>
+        <ul>
+          {practices}
+        </ul>
       </MyAccordionItem>;
     })
     return <AccordionSection id='experience' title='Experience'>

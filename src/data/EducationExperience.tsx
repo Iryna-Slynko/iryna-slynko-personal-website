ad4a8b64fda8de3ab49f84e2a2ae7473b22d2ca5
Iryna Slynko <53280378+Iryna-Slynko@users.noreply.github.com>
2021-12-13 22:24:25 +0000

    Update EducationExperience.tsx

--- src/data/EducationExperience.tsx
+++ src/data/EducationExperience.tsx
@@ -7,13 +7,27 @@ export default function EducationExperience(): IEducationExperience[] {
       website: new URL("https://www.ncirl.ie/"),
       diploma: "Level 8. Higher Diploma in Science in Software Development",
       graduationDate: new Date(2022, 3, 30),
-      modules: []
+      stages: [{
+        stageName: "",
+        modules:
+        [{
+        title:"",
+        grade: ""
+      }],
+      } ]
     }, {
       universityName: "Technological University Dublin",
       website: new URL("https://www.tudublin.ie/"),
       diploma: "Bachelor of Science with Honours in Computing with IT Management",
       graduationDate: new Date(2022, 9, 30),
-      modules: []
+      stages: [{
+        stageName: "",
+        modules:
+        [{
+        title:"",
+        grade: ""
+      }],
+    }]
     }
   ];
 }

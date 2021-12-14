4ff12a8c6f7ca599bba7bfc247cc4d706289430c
Iryna Slynko <iryna.slynko@gmail.com>
2021-12-14 01:33:01 +0000

    Add education

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import { IEducationExperience } from '../types/EducationExperience';
 import { AccordionSection } from './AccordionSection';
 import { MyAccordionItem } from "./AccordionItem";
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
 
 interface IProps {
   readonly education: IEducationExperience[],
@@ -12,9 +14,23 @@ interface IProps {
 export default class Education extends React.Component<IProps> {
   render(): React.ReactNode {
     const experience = this.props.education.map(function (education, index) {
+      const stageTabs = education.stages.map(function(stage) {
+        const modules = stage.modules.map(function(m) {
+          return <li>
+            {m.title} {m.grade !== undefined ? " - " + m.grade : "" }
+          </li>;
+        });
+        return <Tab eventKey={stage.stageName} title={stage.stageName}>
+          {modules}
+        </Tab>
+      });
       const date = (education.graduationDate > new Date() ? "expected in " : "") + format(education.graduationDate, "MMM yyyy");
       return <MyAccordionItem date={date} subtitle={education.diploma} title={education.universityName}
-        index={index} prefix='work' />;
+        index={index} prefix='work'>
+          <Tabs>
+          {stageTabs}
+          </Tabs>
+        </MyAccordionItem>;
     });
 
     return <AccordionSection id='education' title='Education'>

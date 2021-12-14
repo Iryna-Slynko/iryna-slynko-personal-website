f5bd5a3eccb46bbf13b54b9e2112a50852f8cea6
Iryna-Slynko <53280378+Iryna-Slynko@users.noreply.github.com>
2021-12-14 23:39:52 +0000

    Add hobbies

--- src/components/Hobbies.tsx
+++ src/components/Hobbies.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import Image from 'react-bootstrap/Image';
+import hobbies from '../images/hobbies.jpg'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Hobbies() {
   return (
     <section id='hobbies'>
+      <Container>
+        <Row className="justify-content-start">
+          <Col className="pb-5 text-left" sm={3} md={2}>
+            <h4>
+              <span>
+                Hobbies
+              </span>
+            </h4>
+          </Col>
+          <Col className='bars'>
+            <Image src={hobbies} fluid  />
+          </Col>
+        </Row>
+      </Container>
+
     </section>
   )
 }

d3b55528375e8ceb89e2783c8cad9552e7755205
Iryna-Slynko <53280378+Iryna-Slynko@users.noreply.github.com>
2021-12-15 00:02:19 +0000

    Add zoom for hobbies

--- src/components/Hobbies.tsx
+++ src/components/Hobbies.tsx
@@ -1,10 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'react-bootstrap/Image';
 import hobbies from '../images/hobbies.jpg'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+function ImageMagnifier({
+  src,
+  width,
+  height,
+  magnifierHeight = 12,
+  magnifierWidth: magnifierWidth = 12,
+  zoomLevel = 4
+}: {
+  src: string;
+  width?: string;
+  height?: string;
+  magnifierHeight?: number;
+  magnifierWidth?: number;
+  zoomLevel?: number;
+}) {
+  const [[x, y], setXY] = useState([0, 0]);
+  const [[imgWidth, imgHeight], setSize] = useState([0, 0]);
+  const [showMagnifier, setShowMagnifier] = useState(false);
+  return (
+    <div
+      style={{
+        position: "relative",
+        height: height,
+        width: width
+      }}
+    >
+      <Image fluid
+        src={src}
+        style={{ height: height, width: width }}
+        onMouseEnter={(e) => {
+          // update image size and turn-on magnifier
+          const elem = e.currentTarget;
+          const { width, height } = elem.getBoundingClientRect();
+          setSize([width, height]);
+          setShowMagnifier(true);
+        }}
+        onMouseMove={(e) => {
+          // update cursor position
+          const elem = e.currentTarget;
+          const { top, left } = elem.getBoundingClientRect();
+
+          // calculate cursor position on the image
+          const x = e.pageX - left - window.pageXOffset;
+          const y = e.pageY - top - window.pageYOffset;
+          setXY([x, y]);
+        }}
+        onMouseLeave={() => {
+          // close magnifier
+          setShowMagnifier(false);
+        }}
+        alt={"img"}
+      />
+
+      <div
+        style={{
+          display: showMagnifier ? "" : "none",
+          position: "absolute",
+
+          // prevent magnifier blocks the mousemove event of img
+          pointerEvents: "none",
+          // set size of magnifier
+          height: `${magnifierHeight}vw`,
+          width: `${magnifierWidth}vw`,
+          // move element center to cursor pos
+          top: `${y - magnifierHeight / 2}px`,
+          left: `${x - magnifierWidth / 2}px`,
+          opacity: "1", // reduce opacity so you can verify position
+          border: "1px solid lightgray",
+          backgroundColor: "white",
+          backgroundImage: `url('${src}')`,
+          backgroundRepeat: "no-repeat",
+
+          //calculate zoomed image size
+          backgroundSize: `${imgWidth * zoomLevel}px ${
+            imgHeight * zoomLevel
+          }px`,
+
+          //calculate position of zoomed image.
+          backgroundPositionX: `${-x * zoomLevel + magnifierWidth / 2}px`,
+          backgroundPositionY: `${-y * zoomLevel + magnifierHeight / 2}px`
+        }}
+      ></div>
+    </div>
+  );
+}
 export default function Hobbies() {
   return (
     <section id='hobbies'>
@@ -18,7 +103,7 @@ export default function Hobbies() {
             </h4>
           </Col>
           <Col className='bars'>
-            <Image src={hobbies} fluid  />
+            <ImageMagnifier src={hobbies}/>
           </Col>
         </Row>
       </Container>

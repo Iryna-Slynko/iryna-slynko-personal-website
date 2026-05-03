dc299417373dcd5efd9d4336bab0a76d12a9221d
Iryna Slynko <iryna.slynko@gmail.com>
2026-05-03 21:37:27 +0100

    Add Dockerfile and Nginx configuration for deployment; enhance tests for skills, projects, and contacts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,19 @@ test('renders my name link', () => {
   const myName = screen.getAllByText(/Iryna Slynko/i);
   expect(myName[0]).toBeInTheDocument();
 });
+test('renders my skills link', () => {
+  render(<App />);
+  const mySkills = screen.getAllByText(/Skills/i);
+  expect(mySkills[0]).toBeInTheDocument();
+});
+test('renders my projects link', () => {
+  render(<App />);
+  const myProjects = screen.getAllByText(/Projects/i);
+  expect(myProjects[0]).toBeInTheDocument();
+});
+test('renders my contacts link', () => {
+  render(<App />);
+  const myContacts = screen.getAllByText(/Contacts/i);
+  expect(myContacts[0]).toBeInTheDocument();
+});
+test

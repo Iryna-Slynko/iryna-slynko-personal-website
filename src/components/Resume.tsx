ceb34b7e04fe1983e84f39f7464087fc862e8840
Iryna Slynko <iryna.slynko@gmail.com>
2026-05-10 20:05:23 +0100

    Remove unneccessary code

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -32,9 +32,7 @@ export default class Resume extends React.Component<IProps, IState> {
   render(): React.ReactNode {
     return (
       <section id='resume'>
-        <Skills skills={this.props.info.skills} selectedSkill={this.state.selectedSkill} onSkillChange={this.onSelectingItem} />
         <Work jobs={this.props.info.jobs} selectedSkill={this.state.selectedSkill} />
-        <Education education={this.props.info.education} />
         <Projects projects={this.props.info.projects} selectedSkill={this.state.selectedSkill} />
       </section>
     )
